c0b3ba3262e5e43a6c41d274b3655718301375dd
coderking <giteshshastri100@gmail.com>
2018-12-13 11:50:32 +0530

    JWPlayer

---
+++ app/src/main/java/com/careeranna/careeranna/data/FreeVideos.java
@@ -0,0 +1,56 @@
+package com.careeranna.careeranna.data;
+
+import java.io.Serializable;
+
+public class FreeVideos implements Serializable {
+
+    String id, video_url, thumbnail, total_view, tags;
+
+    public FreeVideos(String id, String video_url, String thumbnail, String total_view, String tags) {
+        this.id = id;
+        this.video_url = video_url;
+        this.thumbnail = thumbnail;
+        this.total_view = total_view;
+        this.tags = tags;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVideo_url() {
+        return video_url;
+    }
+
+    public void setVideo_url(String video_url) {
+        this.video_url = video_url;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getTotal_view() {
+        return total_view;
+    }
+
+    public void setTotal_view(String total_view) {
+        this.total_view = total_view;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+}
